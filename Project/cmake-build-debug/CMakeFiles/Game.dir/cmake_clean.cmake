cc0b58d8c52133088ad7785c59c1208a807b7de7
Anya <anyata.maksimova@gmail.com>
2021-03-18 20:22:06 +0300

    new files

---
+++ Project/cmake-build-debug/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/Game/Human.cpp.o"
+  "CMakeFiles/Game.dir/Game/Knight.cpp.o"
+  "CMakeFiles/Game.dir/Game/Magician.cpp.o"
+  "CMakeFiles/Game.dir/Game/Monster.cpp.o"
+  "CMakeFiles/Game.dir/Game/Princess.cpp.o"
+  "CMakeFiles/Game.dir/Game/Unit.cpp.o"
+  "CMakeFiles/Game.dir/Game/UnitFactory.cpp.o"
+  "CMakeFiles/Game.dir/main.cpp.o"
+  "Game"
+  "Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
