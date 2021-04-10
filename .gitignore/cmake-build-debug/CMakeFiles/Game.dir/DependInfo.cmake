c42d41fad26f7c8cdbae931e132af42026fc5a43
Anya <anyata.maksimova@gmail.com>
2021-04-10 07:33:04 +0300

    new .gitignore

---
+++ .gitignore/cmake-build-debug/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/any/programs/Game/Game/Human.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Human.cpp.o"
+  "/home/any/programs/Game/Game/Knight.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Knight.cpp.o"
+  "/home/any/programs/Game/Game/Magician.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Magician.cpp.o"
+  "/home/any/programs/Game/Game/Monster.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Monster.cpp.o"
+  "/home/any/programs/Game/Game/Princess.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Princess.cpp.o"
+  "/home/any/programs/Game/Game/Unit.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/Unit.cpp.o"
+  "/home/any/programs/Game/Game/UnitFactory.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/Game/UnitFactory.cpp.o"
+  "/home/any/programs/Game/main.cpp" "/home/any/programs/Game/cmake-build-debug/CMakeFiles/Game.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
